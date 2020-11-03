33aa613285038ccfd2ac731dc78cc95eb32e68d8
Rodrigo Ceron <raceron19@gmail.com>
2020-11-03 16:08:16 -0600

    crud creation with mock data

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,14 +2,10 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/moviesRouter');
+moviesApi(app);
 
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
 
-app.get('/json', function (req, res) {
-  res.json({ hello: 'world' });
-});
 
 app.listen(config.port,
   function () {
