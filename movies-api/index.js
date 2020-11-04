626277e0741cfe2dbd7a45def44a6ad7720c70e1
Rodrigo Ceron <raceron19@gmail.com>
2020-11-04 16:45:43 -0600

    mongo db connection up and running

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/moviesRouter');
+
+
+//body parser
+app.use(express.json());
+
 moviesApi(app);
 
 
