183d46892df039032dce98115217f39efbd159b7
Rodrigo Ceron <raceron19@gmail.com>
2020-11-05 14:39:46 -0600

    middleware for error handling, node debuggin and request validation implemented

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const debug = require('express-debug');
+
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/moviesRouter');
 
+const { logErrors, errorHandler, wrapError } = require('./utils/middleware/errorHandler');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
+debug(app);
 
 moviesApi(app);
+
+app.use(notFoundHandler);
+
+// middlewares
+app.use(logErrors);
+app.use(wrapError);
+app.use(errorHandler);
 
 
 
