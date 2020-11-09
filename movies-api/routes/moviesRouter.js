1a10fab57442edad4df074b92d1896b8ef309a43
Rodrigo Ceron <raceron19@gmail.com>
2020-11-09 10:04:09 -0600

    adding cache in get actions for movies and vercel config for staging env

--- movies-api/routes/moviesRouter.js
+++ movies-api/routes/moviesRouter.js
@@ -6,6 +6,11 @@ const {
   updateMovieSchema,
 } = require('../utils/schemas/moviesSchema');
 const validationHandler = require('../utils/middleware/validationHandler');
+const cacheResponse = require('../utils/cacheResponse');
+const {
+  FIVE_MINUTES_IN_SECONDS,
+  SIXTY_MINUTES_IN_SECONDS,
+} = require('../utils/time');
 
 function moviesApi(app) {
   const router = express.Router();
@@ -14,6 +19,7 @@ function moviesApi(app) {
   const moviesService = new MoviesService();
 
   router.get('/', async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     try {
       const { tags } = req.query;
       const movies = await moviesService.getMovies({ tags });
@@ -30,6 +36,7 @@ function moviesApi(app) {
     '/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
     async function (req, res, next) {
+      cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
       try {
         const { movieId } = req.params;
         const movies = await moviesService.getMovie({ movieId });
